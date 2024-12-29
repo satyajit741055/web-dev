07caef64ac4039398be33f3cc0f7b81f7dba3805
satyajit741055 <satyajit7410@gmail.com>
2024-12-29 15:35:53 +0530

    first practice

---
+++ 1_2_Basics_of_Java/intro.js
@@ -0,0 +1,42 @@
+// var a = 1
+// a = "Hare  Krishna"
+// a = 0.05
+// console.log(a)
+
+
+// let a = 1
+// while(1)
+// {
+//     a++
+// }
+
+
+// let users = ["Satyajit","Krishna","Jai"]
+
+// console.log(users[0])
+
+
+
+// function greet(a){
+//     return "Hellow , " + a;
+// }
+
+// let message = greet("Krishna")
+// console.log(message)
+
+age = 1523;
+
+function canVote(age){
+    if (age>18) {
+        console.log("Yes he can vote")
+    }
+    else{
+        console.log("No he can't ")
+    }
+}
+
+canVote(age)
+
+
+let sum = 2+5+2+2+2+5+4+2+2+5+2+21
+console.log(sum)
